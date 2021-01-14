6b4460002995ae84500bbc04846809cef45df212
ericcornetto <putrapurnawa@gmail.com>
2021-01-14 22:18:57 +0800

    Penambahan backend pada fitur login admin

--- src/ui/admin/login/Login.js
+++ src/ui/admin/login/Login.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const Login = () => {
+
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+
+    const sendTokenRequest = () => {
+        return fetch('http://localhost:3000/api-admin', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username
+            })
+        })
+    }
+
+    const Signin = () => {
+        firestore()
+            .collection('admin')
+            .where('username','==',username)
+            .where('password','==',password)
+            .get()
+            .then((querySnapshot) => {
+                console.log('User is Valid!');
+
+                sendTokenRequest();
+
+                querySnapshot.forEach(documentSnapshot => {
+                    console.log(documentSnapshot.data()['token']);
+
+                    auth()
+                        .signInWithCustomToken(documentSnapshot.data()['token'])
+                        .then(() => {
+                            console.log('User Signed in!');
+                        })
+                        .catch((error) => {
+                            console.log(error.code,error.message);
+                        })
+                })
+            }) 
+    }
+
     return (
         <View style={styles.container}>
             <Text>Login Admin</Text>
