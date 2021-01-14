255c515e0376953c05f1db81d103a605b2cee175
ericcornetto <putrapurnawa@gmail.com>
2021-01-14 19:06:02 +0800

    penambahan back login untuk mahasiswa

--- src/ui/student/login/Login.js
+++ src/ui/student/login/Login.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const Login = () => {
+
+    const [nim, setNim] = useState();
+    const [password, setPassword] = useState();
+    const [token, setToken] = useState();
+
+    const sendTokenRequest = () => {
+        return fetch('http://localhost:3000/api-student', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                uid: nim
+            })
+        })
+    }
+
+    const getTokenFromApi = () => {
+        return fetch(`http://localhost:3000/token/${nim}.json`)
+        .then((response) => response.json())
+        .then((json) => {
+            setToken(json.token)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    }
+
+    function Signin() {
+        firestore()
+            .collection('student')
+            .where('nim', '==', nim)
+            .where('password', '==', password)
+            .get()
+            .then(querySnapshot => {
+                sendTokenRequest();
+                getTokenFromApi();
+
+                auth()
+                    .signInWithCustomToken(token)
+                    .then(() => {
+                        console.log('Sign Successfull')
+                    })
+                    .catch((error) => {
+                        var errorCode = error.code
+                        var errorMessage = error.message
+
+                        console.log(errorCode,errorMessage);
+                    })
+            })
+        
+    }
+
     return (
         <View style={styles.container}>
             <Text>Login</Text>
