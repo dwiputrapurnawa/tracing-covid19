7279ec34a28659538d805efd95072318a01921cd
ericcornetto <putrapurnawa@gmail.com>
2021-01-16 14:52:23 +0800

    penambahan icon pada tab nvagiation

--- src/ui/student/Tab.js
+++ src/ui/student/Tab.js
@@ -3,12 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../student/home/Home';
 import Booking from '../student/booking/Booking';
 import QRCode from '../student/qrcode/QRCode';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBottom() {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if(route.name === 'Home') {
+                        iconName = focused ? 'grid' : 'grid-outline'
+                    } else if (route.name === 'Booking') {
+                        iconName = focused ? 'bookmarks' : 'bookmarks-outline'
+                    } else if (route.name === 'QRCODE') {
+                        iconName = focused ? 'qr-code' : 'qr-code-outline'
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />
+                }
+            })}
+
+            tabBarOptions={{
+                activeTintColor: '#00E8FF',
+                inactiveTintColor: 'gray',
+            }}
+        >
             <Tab.Screen name="Home" component={Home} options={{title: 'Dashboard'}} />
             <Tab.Screen name="Booking" component={Booking} />
             <Tab.Screen name="QRCODE" component={QRCode} />
