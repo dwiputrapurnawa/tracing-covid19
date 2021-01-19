ba74e2eef68ea2c6d3c5afd45cb8e82bdb157f56
audie <audiedevanugraha@gmail.com>
2021-01-19 14:32:26 +0800

    backend news

--- src/ui/admin/news/News.js
+++ src/ui/admin/news/News.js
@@ -1,11 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Button, FlatList, ActivityIndicator, Image } from 'react-native';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import { ScrollView } from 'react-native-gesture-handler';
+
+
+
 
 const News = () => {
+
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState([])
+
+    const db = firestore();
+
+    useEffect(() => {
+        const subscriber = db
+        .collection('berita')
+        .onSnapshot(querySnapshot => {
+            const users = [];
+      
+            querySnapshot.forEach(documentSnapshot => {
+              users.push({
+                ...documentSnapshot.data(),
+                key: documentSnapshot.id,
+              });
+            });
+           
+            setUsers(users);
+            setLoading(false);
+            
+          });
+        return () => subscriber();
+    }, []);
+
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>News Page</Text>
-        </View>
+
+           <FlatList style={styles.container}
+        data={users}
+        renderItem={({ item }) => (
+          <View style={{ height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Title: {item.title}</Text>
+            <Text>Author: {item.author}</Text>
+            <Text>Body: {item.body}</Text>
+            <Image source={{uri:item.image}}/>
+          </View>
+        )}/>
+
     );
 }
 
